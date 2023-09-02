ec173b414233123f65a57eb911b63b472581f3aa
Angel <48069264+velk20@users.noreply.github.com>
2023-09-02 14:21:02 +0300

    JWT token

--- softuni-cubicle-may-2023/src/managers/userManager.js
+++ softuni-cubicle-may-2023/src/managers/userManager.js
@@ -1,6 +1,8 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
 
+const SECRET = 'eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5MzY1MzQ4NCwiaWF0IjoxNjkzNjUzNDg0fQ';
 exports.register = (userData) => User.create(userData);
 
 exports.login = async (username,password) => {
@@ -17,5 +19,12 @@ exports.login = async (username,password) => {
     throw new Error('Cannot find username or password');
   }
 
-  return user;
+  const  payload = {
+    _id: user._id,
+     username: user.username
+  }
+
+  const token = await jwt.sign(payload, SECRET, {expiresIn: '2d'});
+
+  return token;
 }
