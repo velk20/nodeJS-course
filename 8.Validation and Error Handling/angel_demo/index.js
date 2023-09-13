69aaf84d83ff29ed89f32453a93db227dad0d806
Angel <48069264+velk20@users.noreply.github.com>
2023-09-13 10:45:49 +0300

    valdiation'

---
+++ 8.Validation and Error Handling/angel_demo/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const {isAgeValid} = require('./utils/validations');
+const {validateName} = require('./middlewares/middlewares');
+const validator = require('validator');
+const validExpress = require('express-validator');
+const {body, validationResult} = require('express-validator');
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+  res.send(`
+<form method="POST">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name">
+
+   <label for="password">password</label>
+    <input type="password" name="password" id="password">
+    
+    <label for="age">Age</label>
+    <input type="number" name="age" id="age">
+    
+    <input type="submit" value="create">
+</form>`);
+});
+
+app.post('/',
+  validateName,
+  body('password')
+    .isLength({min:3, max:20})
+    .isStrongPassword()
+    .withMessage('Invalid password by express-validator'),
+  (req,res)=>{
+  const {name, age, password} = req.body;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array()[0].msg);
+    }
+
+  if (!isAgeValid(age)) {
+    return res.send('Invalid age');
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    return res.send('Weak password');
+  }
+
+  console.log(name, age);
+  res.send(`Successfully`);
+})
+
+app.listen(5000, () => console.log(`server is listening on port 5000... `));
